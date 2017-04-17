2568ee14dd8cf22f414ac632867c64813c125d1d
guxuefei <xuefeigu@163.com>
2017-04-17 20:08:13 +0800

    Initial Commit

---
+++ 061-- GXFApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  061-- GXFApplication
+//
+//  Created by 顾雪飞 on 17/4/17.
+//  Copyright © 2017年 顾雪飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
